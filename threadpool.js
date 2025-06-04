eaece910dd21644eb3b2356672f7837c7a6d5dc5
mahvec <62505213+mahvec@users.noreply.github.com>
2025-06-04 13:45:44 +0100

    network IO

--- threadpool.js
+++ threadpool.js
@@ -8,20 +8,42 @@
 
 // console.log("last");
 
-const crypto = require("crypto");
+// const crypto = require("crypto");
+
+// const start = Date.now();
+// // crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
+// // crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
+// // crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
+// // crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
+// // console.log("Hash: ", Date.now() - start);
+
+// // process.env.UV_THREADPOOL_SIZE = 6;
+// const MAX_CALLS = 5;
+
+// for (let i = 0; i < MAX_CALLS; i++) {
+//   crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+//     console.log(`Hash: ${i + 1}`, Date.now() - start);
+//   });
+// }
+
+const https = require("https");
 
 const start = Date.now();
-// crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 64, "sha512");
-// console.log("Hash: ", Date.now() - start);
-
-process.env.UV_THREADPOOL_SIZE = 6;
 const MAX_CALLS = 5;
 
 for (let i = 0; i < MAX_CALLS; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Hash: ${i + 1}`, Date.now() - start);
+  const req = https.request("https://www.google.com", (res) => {
+    res.on("data", () => {});
+    res.on("end", () => {
+      console.log(`Request: ${i + 1}`, Date.now() - start);
+    });
   });
+
+  // Handle request errors
+  req.on("error", (err) => {
+    console.error(`Request ${i + 1} failed:`, err.message);
+  });
+
+  // Important: End the request
+  req.end();
 }
